ddf40f8738e858c9c27ae8ad467620040e57a5df
alejandromoises <alejandromoises02@gmail.com>
2021-02-11 00:05:05 -0300

    Desafio clase 9

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const router = express.Router()
 app.use(express.json());
 const fs = require("fs");
 const puerto = 8080;
@@ -61,12 +62,17 @@ class Archivo {
 const miArchivo = new Archivo("productos");
 //Manejo de archivos
 
-app.get("/", (req, res) => {
+
+
+//Server
+router.get("/", (req, res) => {
   console.log("Consulta realizada a /");
   res.send("Principal");
 });
 
-app.get("/api/productos", (req, res) => {
+
+
+router.get("/productos", (req, res) => {
   async function getProductos() {
     const productos = await miArchivo.leer();
     const resp = {
@@ -85,7 +91,7 @@ app.get("/api/productos", (req, res) => {
     });
 });
 
-app.get("/api/productos/:id", (req, res) => {
+router.get("/productos/:id", (req, res) => {
   const id = parseInt(req.params.id);
   async function getProductos() {
     const productos = await miArchivo.leer();
@@ -103,7 +109,7 @@ app.get("/api/productos/:id", (req, res) => {
   }
 });
 
-app.post("/api/productos", (req, res) => {
+router.post("/productos", (req, res) => {
   try {
     const { title, price, thumbnail } = req.body;
     miArchivo.guardar(title, price, thumbnail)
@@ -112,8 +118,69 @@ app.post("/api/productos", (req, res) => {
     console.log(error);
     res.sendStatus(500);
   }
-});
+})
+
+
+
+router.patch("/productos/actualizar/:id", (req,res)=>{
+  const id = parseInt(req.params.id);
+  const { title, price, thumbnail } = req.body;
+
+  async function getProductos() {
+    const productos = await miArchivo.leer();
+    return productos;
+  }
+  getProductos()
+    .then((productos) => {
+      const producto = productos.find(producto => producto.id === id)
+      if(!producto){
+        res.sendStatus(404)
+      }
+      producto.title =title
+      producto.price =price
+      producto.thumbnail =thumbnail
+      productos = productos.filter(element =>{
+        if(element.id === producto.id)return producto
+        else return element
+      })
+      res.send(producto)
+    })
+})
+
+
+router.delete("/productos/borrar/:id", (req,res)=>{
+  console.log("entro");
+  const id = parseInt(req.params.id);
+  console.log(id);
+
+  async function getProductos() {
+    const productos = await miArchivo.leer();
+    return productos;
+  }
+  getProductos()
+    .then((productos) => {
+      console.log(productos);
+      const producto = productos.find(producto => producto.id === id)
+      if(!producto){
+        res.sendStatus(404)
+      }
+      productos = productos.filter(element => element.id ==! id)
+      console.log(productos);
+      
+      res.sendStatus(200)
+    })
+})
+
+
+router.get("/ingresar", (req, res) => {
+res.sendFile(__dirname + "/index.html")  
+})
+
+app.use("/api", router)
+
+
 
 app.listen(puerto, () => {
   console.log(`Servidor esuchando puerto ${puerto}`);
 });
+//Server

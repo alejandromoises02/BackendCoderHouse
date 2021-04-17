b3c585ce39dde87b5c384bf67f6225cbb6ff9309
alejandromoises <alejandromoises02@gmail.com>
2021-04-17 00:52:15 -0300

    Desafio 23

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const app = require("express")();
-const chatModel = require('./models/chat.mongoose');
-const mongoose = require('mongoose');
+const chatModel = require("./models/chat.mongoose");
+const mongoose = require("mongoose");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-var moment = require('moment'); 
-moment().format(); 
+var moment = require("moment");
+const { schema, normalize } = require("normalizr");
+moment().format();
 const puerto = 8080;
 let users = [];
 
+const author = new schema.Entity("author")
+const msg = new schema.Entity("text")
+const date = new schema.Entity("date")
+const nuevoMsg = new schema.Entity("nuevoMensaje",{
+  author,
+  msg,
+  date
+},{idAttribute: "id"});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -16,45 +25,55 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log(`se conecto el usuario ${socket.id}`);
 
-  socket.join(socket.id)
+  socket.join(socket.id);
   socket.on("session", (payload) => {
-    if (!users.includes(payload.mail)) {
-      users.push(payload.mail);
+    console.log(payload);
+    if (!users.includes(payload)) {
+      //users.push(payload.mail);
+      users.push(payload); //nl
     }
     io.to(socket.id).emit("userId", {
-      sessionId: payload.mail,
+      sessionId: payload,
       users,
     });
   });
 
   socket.on("message", (payload) => {
     let nuevoMensaje = {
-      from: payload.mail,
-      msg: payload.msg,
-      date: moment().format('L')
-    }
-    io.emit("message", nuevoMensaje);
-    const chatSaved = new chatModel(nuevoMensaje)
-    chatSaved.save()
+      author: {
+        id: payload.mail,
+        nombre: payload.nombre,
+        apellido: payload.apellido,
+        edad: payload.edad,
+        alias: payload.alias,
+        avatar: payload.avatar,
+      },
+      text: payload.msg,
+      date: moment().format("L"),
+    };
+
+const normalizedMsg = normalize(nuevoMensaje, nuevoMsg)
+
+
+    io.emit("message", normalizedMsg);
+
+    console.log(normalizedMsg);
+
+    const chatSaved = new chatModel(nuevoMensaje);
+    chatSaved.save();
   });
-
-
 });
 
 io.on("disconnect", (socket) => {
   console.log("se desconecto");
 });
 
-
-
 http.listen(puerto, () => {
-  mongoose.connect('mongodb://localhost:27017/test-mongoose',
-      {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-      }
-  )
-      .then( () => console.log(`Servidor esuchando puerto ${puerto}`))
-      .catch( (err) => console.log(err));
-})
-
+  mongoose
+    .connect("mongodb://localhost:27017/test-mongoose", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log(`Servidor esuchando puerto ${puerto}`))
+    .catch((err) => console.log(err));
+});
--- models/chat.mongoose.js
+++ models/chat.mongoose.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const chatCollection = 'chat'
+const chatCollection = "chat";
 
 const ChatSchema = new mongoose.Schema({
-    from: { type: String, required: true, max: 50 },
-    msg: { type: String, required: true, max: 70 },
-    date: { type: String, required: true, max: 70 },
-})
+  author: {
+    id: { type: String, required: true, max: 50 },
+    nombre: { type: String, required: true, max: 50 },
+    apellido: { type: String, required: true, max: 50 },
+    edad: { type: String, required: true, max: 50 },
+    alias: { type: String, required: true, max: 50 },
+    avatar: { type: String, required: true, max: 50 },
+  },
+  text: { type: String, required: true, max: 70 },
+  date: { type: String, required: true, max: 70 },
+});
 
-module.exports = mongoose.model(chatCollection, ChatSchema)
+module.exports = mongoose.model(chatCollection, ChatSchema);
